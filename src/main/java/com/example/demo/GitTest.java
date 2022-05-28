dab7d59a5f66177f06ed2a3304442cd0ed5745ff
Zhengyang <zyli1029@gmail.com>
2022-05-28 21:30:37 +0800

    push commit

--- src/main/java/com/example/demo/GitTest.java
+++ src/main/java/com/example/demo/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hot fix testtttttttt");
         System.out.println("hot fix test1");
+        System.out.println("push test1");
     }
 }
